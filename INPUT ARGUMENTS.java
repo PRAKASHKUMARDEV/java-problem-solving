cc445fb1b374df064b4f6acf8c1482bb00827867
PRAKASHKUMAR <105591571+PRAKASHKUMARDEV@users.noreply.github.com>
2022-05-25 01:40:51 -0700

    Update INPUT ARGUMENTS.java

--- INPUT ARGUMENTS.java
+++ INPUT ARGUMENTS.java
@@ -1,12 +1,13 @@
 import java.util.*;
 import java.io.*;
 class  input_arguments
-{                                                       //we have to give arguments after complie .java files in cmd prompt 
+{                                                       /*we have to give arguments after complie .java files in cmd prompt  its programe use to caluluate your 
+	                                                  family memers */
 	public static void main(String[] args) {
     for(String a:args){
 			System.out.println(" your family members name  "+"  " +a);
 		}
 		System.out.println("totalyour  family members"+"   "+ (args.length));
 		}
-	}
+}
 
