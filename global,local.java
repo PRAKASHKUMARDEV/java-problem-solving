ec4511efaaf5cc371f3c056dd5663384029eba03
PRAKASHKUMAR <105591571+PRAKASHKUMARDEV@users.noreply.github.com>
2022-05-28 07:13:56 +0530

    Update global,local.java

--- global,local.java
+++ global,local.java
@@ -1,14 +1,26 @@
-class global{
-	static int a=10;//instance variables 
-	static int b=20;
-	/*above programe u will get error because if u want to acces any instance variables 
+
+package programe;
+import
+/*public static class global_local {       
+int a;
+int b;
+a=10;b=20;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("your global varible is"+a+" "+b);
+		}
+	}*/
+/*above programe u will get error because if u want to acces any instance variables 
 (class varibles )u need object even class function also cannot able to acces */
 
 /*in local variables means we can acces only in side the fucntion */
 
 //let we understand the concept deeply ok let we check it //
 
-	public static void main(String[] args ) {
+	class global{
+	static int a=10;//instance variables 
+	static int b=20;
+        public static void main(String[] args ) {
 		System.out.println("your entered string ="+a+" "+b);
 		int a=100;//local variables 
 		int b=500;
