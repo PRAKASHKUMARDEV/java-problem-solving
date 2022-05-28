780748e838868cee52eef579f837070c28e7558d
PRAKASHKUMAR <105591571+PRAKASHKUMARDEV@users.noreply.github.com>
2022-05-28 08:04:28 +0530

    Create realational .op .java

---
+++ realational .op .java
@@ -0,0 +1,25 @@
+
+import java.util.*;
+class input{
+	
+	
+	public static void main(String[] args ) {
+	
+		System.out.println("realtional  operaters <= < > >= ");
+		System.out.println("equality check == != ");
+		/*realtional operater always gives true or false we 
+		are using operater majorly loops and select statements
+		if else and for */
+		
+		Scanner in=new Scanner(System.in);
+		
+   int a=in.nextInt();
+   int b=in.nextInt();
+
+System.out.println(a>b);
+System.out.println(b==a);
+
+		
+		
+	}
+}
