93b9a04d7a99e2ea8058b8d34e819cf19af81b82
PRAKASHKUMAR <105591571+PRAKASHKUMARDEV@users.noreply.github.com>
2022-05-28 07:24:43 +0530

    Create escape_sequence .java

---
+++ escape_sequence .java
@@ -0,0 +1,13 @@
+class escape{
+	
+	
+	public static void main(String[] args ) {
+	
+		System.out.println("Java is\bs a programming \n language and computing platform \t first released by Sun Microsystem");
+		
+		// /r -means cursore comes to firts line with deleting  before text 
+		// // single / indroduced 
+		// /n new line 
+		// /b deleted one charecter 
+	}
+}
