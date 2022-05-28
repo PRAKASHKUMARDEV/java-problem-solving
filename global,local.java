9a97ac841d4672408573b426ca4dc0a10113bc16
PRAKASHKUMAR <105591571+PRAKASHKUMARDEV@users.noreply.github.com>
2022-05-28 07:12:04 +0530

    Update global,local.java

--- global,local.java
+++ global,local.java
@@ -1,7 +1,13 @@
 class global{
 	static int a=10;//instance variables 
 	static int b=20;
-	
+	/*above programe u will get error because if u want to acces any instance variables 
+(class varibles )u need object even class function also cannot able to acces */
+
+/*in local variables means we can acces only in side the fucntion */
+
+//let we understand the concept deeply ok let we check it //
+
 	public static void main(String[] args ) {
 		System.out.println("your entered string ="+a+" "+b);
 		int a=100;//local variables 
