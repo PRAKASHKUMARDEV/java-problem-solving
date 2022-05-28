9d602aa00544e3c2643ad3bb49a1f1c80d004248
PRAKASHKUMAR <105591571+PRAKASHKUMARDEV@users.noreply.github.com>
2022-05-28 18:01:50 +0530

    Create notes.java

---
+++ notes.java
@@ -0,0 +1,14 @@
+//why we using package package means 
+import java,lang.util.scanner;;//its not a #include already ellam jre inbuild panniduchu example below 
+class{
+  java.lang.util.scanner();//eppadi eyuthrathukku pathila mela eyuthi vachikurom import thats all 
+  
+}//entha class la tranning kulla thokki podanum examples 
+math.sqrt(5);//ethukku importla java.math.*;//
+sqrt();//eppadi use pannalam 
+//its means ethu static class 
+package training;//nan eyuthana ellathyium traiining kulla podu your conveneince 
+conflict //kuyappam varakodathunu 
+  pro.java;
+pro.java so rendum ora class name so diffrent package la potta kuyappam varathu;
+
