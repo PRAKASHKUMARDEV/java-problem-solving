5e021ce43bdfa692d57b8a7254e7b3ed6fb022c8
PRAKASHKUMAR <105591571+PRAKASHKUMARDEV@users.noreply.github.com>
2022-05-28 07:11:04 +0530

    Create global,local.java

---
+++ global,local.java
@@ -0,0 +1,12 @@
+class global{
+	static int a=10;//instance variables 
+	static int b=20;
+	
+	public static void main(String[] args ) {
+		System.out.println("your entered string ="+a+" "+b);
+		int a=100;//local variables 
+		int b=500;
+		System.out.println("your entered string ="+a+" "+b);
+		
+	}
+}
