67befe189987186fe92b3cea345ae941842dd192
PRAKASHKUMAR <105591571+PRAKASHKUMARDEV@users.noreply.github.com>
2022-05-29 08:36:08 +0530

    Create wrapping notes.java

---
+++ wrapping notes.java
@@ -0,0 +1,7 @@
+//warpping class - why this java developers craeting warpper class 
+//for exmaple if u want to use collections EXAMPLE ARRAYLIST<OBJECT OR CLASS NAME >=NEW ARRAYLIST<OBJECT OR CLASS >();
+//intiger  i2 = integer.valueof(15)>boxing ;f3 open it any class defenition 
+//int j =i2.intvalue(); un boxing 
+//int i =50; beackgroung boxing integer.valueof(50) auto boxing
+
+
