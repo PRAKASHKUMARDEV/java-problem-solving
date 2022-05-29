ee633207d9aa6044f3cd4dfd54042f9ec833ed3c
PRAKASHKUMAR <105591571+PRAKASHKUMARDEV@users.noreply.github.com>
2022-05-29 10:55:35 +0530

    Create rules.java

---
+++ rules.java
@@ -0,0 +1,2 @@
+oru classku entha class la  vena object create pannalam but antha classsla antha class OBJECT VACHI mattum than antha function variables accec panna mudyium 
+private kuduthuta athuvum mudiyathu so datas private ands functions public kodukalam so producted kodutha inheritance accces pannikalam
