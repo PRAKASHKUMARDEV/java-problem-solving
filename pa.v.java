901da7f84b79f47632095e30dd6b29b75ceb04ca
PRAKASHKUMAR <105591571+PRAKASHKUMARDEV@users.noreply.github.com>
2022-06-01 22:06:15 +0530

    Create pa.v.java

---
+++ pa.v.java
@@ -0,0 +1,33 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.util.*;
+public class Main
+{
+  public static void main (String[]args)
+  {
+
+    for (int i = 1; i <= 5; i++)
+      {
+	for (int j = 1; j <= 5; j++)
+	  {
+	    if (i ==1 || i == 5||j==1||j==5||i==j)
+	      {
+		System.out.print ("*");
+	      }
+	    else
+	      {
+		System.out.print (" ");
+	      }
+	  }
+	System.out.println ();
+      }
+  }
+
+
+
+}
